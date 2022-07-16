c99b4898c145ee202adfae95d75abaf9ee5fcbb8
Vivi <shoter998@gmail.com>
2022-07-16 22:39:12 +0200

    changed shape of the fog

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -7,26 +7,28 @@ public class MoveCamera : MonoBehaviour
     public GameObject player;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float posX = transform.position.x - player.transform.position.x;
         float posY = transform.position.y - player.transform.position.y;
         print(posX);
+        Vector3 newPos = transform.position;
         if(posX >= 5)
         {
-            transform.position = new Vector3(transform.position.x - 10, transform.position.y, -10);
+            newPos = new Vector3(transform.position.x - 10, transform.position.y, -10);
         }
         if(posX <= -5)
         {
-            transform.position = new Vector3(transform.position.x + 10, transform.position.y, -10);
+            newPos = new Vector3(transform.position.x + 10, transform.position.y, -10);
         }
         if(posY >= 5)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 10, -10);
+            newPos = new Vector3(transform.position.x, transform.position.y - 10, -10);
         }
         if(posY <= -5)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 10, -10);
+            newPos = new Vector3(transform.position.x, transform.position.y + 10, -10);
         }
+        transform.position = newPos;
     }
 }
