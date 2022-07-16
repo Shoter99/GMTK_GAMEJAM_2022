98181714fdb889d6a3a314cba6fd837a6376be6b
Vivi <shoter998@gmail.com>
2022-07-16 21:21:38 +0200

    Added game enter animation

--- Assets/MapGeneration/Scripts/Destroyer.cs
+++ Assets/MapGeneration/Scripts/Destroyer.cs
@@ -5,6 +5,7 @@ public class Destroyer : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(!other.CompareTag("Player") && !other.CompareTag("Enemy"))
         Destroy(other.gameObject);
 
     }
