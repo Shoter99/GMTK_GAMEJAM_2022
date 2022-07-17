77c4b36eef45edcd0be32a31346876ed4b274927
Vivi <69056247+Shoter99@users.noreply.github.com>
2022-07-17 17:20:15 +0200

    Added hud elements

---
+++ Assets/hud/clickTriggered.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class clickTriggered : MonoBehaviour
+{
+    public GameObject EventSystem = null;
+    public bool isClicked;
+    public UiManager receiver;
+
+    
+    void Start() 
+    {
+        receiver = EventSystem.GetComponent<UiManager>();
+        this.GetComponent<Animator>().Play("unclicked");
+        isClicked = false;
+        receiver.selectedCount = 0;  
+
+
+    }
+
+
+    public void clicked(){
+        Debug.Log(isClicked);
+        if (isClicked) {
+            this.unclick();
+        }
+        else {
+            if (receiver.selectedCount < 2) {
+            this.GetComponent<Animator>().Play("clicked");
+            this.isClicked = true;
+            receiver.receiveClickedAction(this.gameObject);
+            receiver.selectedCount++;
+
+            }
+        }
+    }
+
+    public void unclick(){
+        if (isClicked){
+            this.GetComponent<Animator>().Play("unclicked");
+            receiver.selectedCount--;
+            isClicked = false;
+        }        
+    }
+}
