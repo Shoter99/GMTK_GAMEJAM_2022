0bb57f873efa8799bd4bbe35141dac911886dad3
Vivi <shoter998@gmail.com>
2022-07-17 11:18:44 +0200

    Added campfire and post processing

--- Assets/MapGeneration/Scripts/Destroyer.cs
+++ Assets/MapGeneration/Scripts/Destroyer.cs
@@ -5,7 +5,7 @@ public class Destroyer : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(!other.CompareTag("Player") && !other.CompareTag("Enemy"))
+        if(!other.CompareTag("Player") && !other.CompareTag("Enemy") && !other.CompareTag("Respawn"))
         Destroy(other.gameObject);
 
     }
